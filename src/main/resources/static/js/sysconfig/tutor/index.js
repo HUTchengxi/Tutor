8140a2217feea8e6aaa613b6cc601a80dbe76951
殷纪民 <yinjimin@iboxpay.com>
2018-03-31 10:59:01 +0800

    实现后台的退出登录和个人中心融合

--- src/main/resources/static/js/sysconfig/tutor/index.js
+++ src/main/resources/static/js/sysconfig/tutor/index.js
@@ -11,13 +11,21 @@ $(function() {
             url: "/usermain_con/getuserinfo",
             dataType: "json",
             success: function(data) {
-                $(".layui-layout-right img.layui-nav-img").attr("src", data.imgsrc);
-                $(".layui-layout-right span.author").text(data.nickname);
+                //最上面更新
+                $(".layui-header .top_menu li.userinfo img").attr("src", data.imgsrc);
+                $(".layui-header .top_menu li.userinfo cite").text(data.nickname);
+                //左侧更新
+                $(".layui-side .user-photo img").attr("src", data.imgsrc);
+                $(".layui-side .user-photo p span").text(data.nickname);
+                //锁屏更新
+                $("#lock-box img").attr("src", data.imgsrc);
+                $("#lock-box #lockUserName").text(data.nickname);
             },
             error: function(xhr, status){
                 window.alert("后台环境异常导致无法获取用户数据，请稍后再试");
+                console.log(xhr);
             }
-        })
+        });
     };
     async_gettutorinfo();
 
@@ -46,197 +54,4 @@ $(function() {
     };
     $(document).on("click", ".top_menu .layui-nav-item dl dd a.logoff", logoff_btn);
 
-    /**
-     * 异步获取历史版本更新数据
-     */
-    var async_getlognew = function(){
-
-        $(".syslog table tbody").empty();
-        $.ajax({
-            async: true,
-            type: "post",
-            url: "/publishlog_con/getlognew",
-            dataType: "json",
-            success: function(data){
-                var count = data.count;
-                console.log(data);
-                if(count == 0){
-                    return ;
-                }
-                var ptype = null;
-                var pversion = null;
-                var ptime = null;
-                $.each(data, function(index, item){
-                   ptype = item.ptype;
-                   pversion = item.pversion;
-                   ptime = item.ptime;
-                   var descript = item.descript;
-                   $(".syslog table tbody").append("<tr>\n" +
-                       "                            <td align=\"left\">\n" +
-                       "                                <p>* "+descript+"</p>\n" +
-                       "                            </td>\n" +
-                       "                        </tr>");
-                });
-                $(".syslog table thead tr td h4 span.pub-version").text(pversion);
-                $(".syslog table thead tr td h4 span.pub-type").text(ptype);
-                $(".syslog table thead tr td h4 span.pub-time").text(ptime);
-            },
-            error: function(xhr, status){
-                window.alert("后台环境异常导致无法获取版本更新数据，请稍后再试");
-                window.console.log(xhr);
-            }
-        });
-    };
-    async_getlognew();
-
-    /**
-     * 异步获取当前家教的所有常用链接数据
-     */
-    // var async_getbtnslist = function(){
-    //
-    //     $(".news_search .tutor-btns").remove();
-    //     $.ajax({
-    //         async: true,
-    //         type: "post",
-    //         url: "/tutorbtns_con/getbtnslist",
-    //         dataType: "json",
-    //         success: function(data){
-    //             var count = data.count;
-    //             if(count == 0){
-    //                 return;
-    //             }
-    //             $.each(data, function(index, item){
-    //                var id = item.id;
-    //                var name = item.name;
-    //                var url = item.url;
-    //                $(".news_search").prepend("<div class=\"layui-inline tutor-btns\">\n" +
-    //                    "                    <a class=\"layui-btn audit_btn layui-btn-normal\" href='/forward_con/"+url+"'>"+name+"</a>\n" +
-    //                    "                    <i class=\"layui-icon remove\" data-id='"+id+"'>&#x1006;</i>\n" +
-    //                    "                </div>");
-    //             });
-    //         },
-    //         error: function(xhr, status){
-    //             window.alert("后台环境异常导致无法获取常用链接数据，请稍后再试");
-    //             window.console.log(xhr);
-    //         }
-    //     });
-    // };
-    // async_getbtnslist();
-
-    /**
-     * 异步获取今日我的课程收藏总次数
-     */
-    var async_getcollectcount = function(){
-
-        $.ajax({
-            async: true,
-            type: "post",
-            url: "/coursecollect_con/getcollectcount",
-            dataType: "json",
-            success: function(data){
-                var count = data.count;
-                if(count >= 999) {
-                    $(".userAll cite").text("999+");
-                }
-                else if(count == 0){
-                    $(".userAll cite").text("--");
-                }
-                else{
-                    $(".userAll cite").text(data.count);
-                }
-            },
-            error: function(xhr, status){
-                window.alert("后台环境异常导致无法获取今日课程收藏数量，请稍后再试");
-                window.console.log(xhr);
-            }
-        });
-    };
-    async_getcollectcount();
-
-    /**
-     * 异步获取今日我的课程评论总次数
-     */
-    var async_getcommandcount = function(){
-
-        $.ajax({
-            async: true,
-            type: "post",
-            url: "/coursecommand_con/getcommandcount",
-            dataType: "json",
-            success: function(data){
-                var count = data.count;
-                if(count >= 999) {
-                    $(".waitNews cite").text("999+");
-                }
-                else if(count == 0){
-                    $(".waitNews cite").text("--");
-                }
-                else{
-                    $(".waitNews cite").text(data.count);
-                }
-            },
-            error: function(xhr, status){
-                window.alert("后台环境异常导致无法获取今日课程评价数量，请稍后再试");
-                window.console.log(xhr);
-            }
-        });
-    };
-    async_getcommandcount();
-
-    /**
-     * 异步获取今日我的课程下单总次数
-     */
-    var async_getordercount = function(){
-
-        $.ajax({
-            async: true,
-            type: "post",
-            url: "/courseorder_con/getordercount",
-            dataType: "json",
-            success: function(data){
-                var count = data.count;
-                if(count >= 999) {
-                    $(".imgAll cite").text("999+");
-                }
-                else if(count == 0){
-                    $(".imgAll cite").text("--");
-                }
-                else{
-                    $(".imgAll cite").text(data.count);
-                }
-            },
-            error: function(xhr, status){
-                window.alert("后台环境异常导致无法获取今日课程下单数量，请稍后再试");
-                window.console.log(xhr);
-            }
-        });
-    };
-    async_getordercount();
-
-    /**
-     * 异步获取今日我的课程评分
-     */
-    var async_getvisitcount = function(){
-
-        $.ajax({
-            async: true,
-            type: "post",
-            url: "/coursecommand_con/getscoreavg",
-            dataType: "json",
-            success: function(data){
-                var count = data.count;
-                if(count == "null"){
-                    $(".newMessage cite").text("--");
-                }
-                else{
-                    $(".newMessage cite").text(data.count);
-                }
-            },
-            error: function(xhr, status){
-                window.alert("后台环境异常导致无法获取今日课程访问数量，请稍后再试");
-                window.console.log(xhr);
-            }
-        });
-    };
-    async_getvisitcount();
 });
