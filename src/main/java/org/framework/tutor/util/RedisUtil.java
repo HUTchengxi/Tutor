2cb20ca95c1135d4fa51bda9f3a7d76f417fc6f6
殷纪民 <yinjimin@iboxpay.com>
2018-04-30 10:57:08 +0800

    系统调优：初次使用Redis缓存：目前只缓存了我的帖子数量，键名采取：username_property格式（因为Redis的map的值只能是String类型）

---
+++ src/main/java/org/framework/tutor/util/RedisUtil.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2011-2013 ShenZhen iBoxpay Information Technology Co. Ltd.
+ *
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of iBoxPay Company of China.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the contract agreement you entered into with iBoxpay inc.
+ *
+ *
+ */
+package org.framework.tutor.util;
+
+/**
+ * @author yinjimin
+ * @Description: redis二级缓存测试
+ * @date 2018年04月28日
+ */
+public class RedisUtil {
+}
